229e79762b4e132dc7e93cbc79acf6b5121ead81
Andrea Fioroni <andreafioroni@Andreas-MBP.lan>
2017-04-12 19:46:13 +0200

    master: new talker file

--- src/talker.py
+++ src/talker.py
@@ -10,14 +10,28 @@
 import rospy
 from std_msgs.msg import String
 
-def talker():
+def talker():                 # TODO: set proper topic name
     pub = rospy.Publisher('rospibot_network', String, queue_size=10) # publish on rospibot_network topic
-    rospy.init_node('rospibot_network_talker', anonymous=True) # init a rospibot_network_talker node
+    rospy.init_node('ros-joy-controller', anonymous=True) # init a rospibot_network_talker node
     rate = rospy.Rate(10) # 10hz
+
+    def_speed = 50
+
     while not rospy.is_shutdown(): # loop until shutdown
         dir_str = raw_input("Choose direction: ")
+
+        if dir_str == "w" :     # w = move forward
+            pub.publish("")
+        elif dir_str == "a" :   # a = turn left
+            pub.publish("")
+        elif dir_str == "s" :   # s = move backward
+            pub.publish("")
+        elif dir_str == "d" :   # d = turn right
+            pub.publish("")
+        elif dir_str == "x" :   # x = stop
+            pub.publish("")
+            
         rospy.loginfo(dir_str)
-        pub.publish(dir_str) # publish on the topic
         rate.sleep()
 
 if __name__ == '__main__':
