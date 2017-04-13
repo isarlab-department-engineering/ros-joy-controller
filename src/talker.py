8d6ab7757a2f9ee337d0eb7bb2845b79bc9e591f
andrea <andrifiore@gmail.com>
2017-04-13 12:05:36 +0200

    master: now talker.py publishes an array on cmd topic

--- src/talker.py
+++ src/talker.py
@@ -8,30 +8,42 @@
 #
 
 import rospy
-from std_msgs.msg import String
+from std_msgs.msg import Int16MultiArray
+import numpy as np
 
-def talker():                 # TODO: set proper topic name
-    pub = rospy.Publisher('rospibot_network', String, queue_size=10) # publish on rospibot_network topic
-    rospy.init_node('ros-joy-controller', anonymous=True) # init a rospibot_network_talker node
+def talker():    
+    pub = rospy.Publisher('cmd', Int16MultiArray, queue_size=10) # publish on motor_hat's cmd topic
+    rospy.init_node('ros-joy-controller', anonymous=True) # init a ros-joy-controller node
     rate = rospy.Rate(10) # 10hz
 
-    def_speed = 50
+    m1_speed = -150
+    m2_speed = -133
+
+    def send(values):
+        values = [-v for v in values]  # hack
+        msg = Int16MultiArray()
+        msg.data = values
+        pub.publish(msg)
 
     while not rospy.is_shutdown(): # loop until shutdown
         dir_str = raw_input("Choose direction: ")
 
         if dir_str == "w" :     # w = move forward
-            pub.publish("")
+	    send([m1_speed,m2_speed,0,0])
+	    rospy.loginfo(" - Move Forward")
         elif dir_str == "a" :   # a = turn left
-            pub.publish("")
+            send([0,m2_speed,0,0])
+	    rospy.loginfo(" - Turn Left")
         elif dir_str == "s" :   # s = move backward
-            pub.publish("")
+            send([-m1_speed,-m2_speed,0,0])
+	    rospy.loginfo(" - Move Backward")
         elif dir_str == "d" :   # d = turn right
-            pub.publish("")
+            send([m1_speed,0,0])
+	    rospy.loginfo(" - Turn Right")
         elif dir_str == "x" :   # x = stop
-            pub.publish("")
+	    send([0,0,0,0])
+	    rospy.loginfo(" - Stop")
             
-        rospy.loginfo(dir_str)
         rate.sleep()
 
 if __name__ == '__main__':
